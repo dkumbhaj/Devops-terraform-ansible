59a253ae4c2732a547124c419c9bd328a020934c
dkumbhaj <139111899+dkumbhaj@users.noreply.github.com>
2023-07-12 15:08:46 +0530

    Update server.tf

--- server.tf
+++ server.tf
@@ -13,12 +13,12 @@ region = "us-east-1"
 resource "aws_instance" "myawsserver" {
   ami = "ami-0b0dcb5067f052a63"
   instance_type = "t2.micro"
-  key_name = "doorwa-keypair"
+  key_name = "Doorwa-keypair"
 
   tags = {
     Name = "Doorwa-DevOps-batch-server"
     env = "Production"
-    owner = "Raman Khanna"
+    owner = "Doorwa Kumbhaj"
   }
   provisioner "local-exec" {
     command = "echo The servers IP address is ${self.public_ip} && echo ${self.public_ip} > /tmp/inv"
